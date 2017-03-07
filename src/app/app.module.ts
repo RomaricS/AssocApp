5f736a1702925759ce41380ccb9fb8566222fee4
RomaricS <romaricboss@gmail.com>
2017-03-07 13:56:52 +0100

    Chat en place, code de push en place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,8 @@ import { User } from '../providers/user';
 import { MembresService } from '../services/membres/membres.services';
 import { EditUserPage } from '../pages/edit-user/edit-user';
 
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+
 
 export const firebaseConfig = {
     apiKey: "AIzaSyBn6l4S0xBJQhAUh3GQAu6B77kV39Qaku4",
@@ -28,6 +30,24 @@ export const firebaseConfig = {
     databaseURL: "https://lshare-30444.firebaseio.com",
     storageBucket: "lshare-30444.appspot.com",
     messagingSenderId: "617794774533"
+};
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': 'b7dcdedc'
+  },
+  'push': {
+    'sender_id': '617794774533',
+    'pluginConfig': {
+      'ios': {
+        'badge': true,
+        'sound': true
+      },
+      'android': {
+        'iconColor': '#343434'
+      }
+    }
+  }
 };
 
 @NgModule({
@@ -47,7 +67,8 @@ export const firebaseConfig = {
   ],
   imports: [
     IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -3,6 +3,11 @@ import { NavController, NavParams } from 'ionic-angular';
 import { User } from '../../providers/user';
 import { FirebaseListObservable } from 'angularfire2';
 
+import {
+  Push,
+  PushToken
+} from '@ionic/cloud-angular';
+
 @Component({
   selector: 'page-chat',
   templateUrl: 'chat.html'
@@ -13,11 +18,28 @@ messages: FirebaseListObservable<any>;
 message:string;
 mesInfos: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public user:User) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public user:User,public push: Push) {
     user.loadProfile();
     this.messages = user.getMessages();
     this.mesInfos = user.userfromdb;
+
+
+//notif
+      this.push.register().then((t: PushToken) => {
+      return this.push.saveToken(t);
+    }).then((t: PushToken) => {
+      console.log('Token saved:', t.token);
+    });
+
+//suscribe to notif observable
+    this.push.rx.notification()
+  .subscribe((msg) => {
+    alert(msg.title + ': ' + msg.text);
+  });
+
   }
+
+
 
 sendMessage(nom:string, pren:string, mail:string){
   //console.log(this.message);
